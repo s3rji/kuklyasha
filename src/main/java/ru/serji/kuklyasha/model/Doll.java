e27862b23a5731c1b25570a56b6c5e6b0e0934b0
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-06-05 21:06:26 +0300

    add doll entity in model

---
+++ src/main/java/ru/serji/kuklyasha/model/Doll.java
@@ -0,0 +1,49 @@
+package ru.serji.kuklyasha.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Doll")
+public class Doll extends BaseEntity {
+
+    @Column(name = "name", nullable = false, unique = true)
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    @NotBlank()
+    @Size(min = 10)
+    private String description;
+
+    @Column(name = "price", nullable = false, precision = 6, scale = 2)
+    @NotNull
+    @Range(min = 1, max = 100000)
+    @Digits(integer = 6, fraction = 2)
+    private BigDecimal price;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
+    private LocalDateTime dateTime;
+}
---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS doll;
+
+CREATE TABLE doll
+(
+    id               SERIAL PRIMARY KEY,
+    name             VARCHAR UNIQUE                    NOT NULL,
+    description      VARCHAR                           NOT NULL,
+    price            NUMERIC(8,2)                      NOT NULL,
+    image            VARCHAR                           NOT NULL,
+    date_time        TIMESTAMP           DEFAULT now() NOT NULL
+);
