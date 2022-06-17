63bab88115fed281747efcdd18b46ac30e39e91d
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-06-17 21:55:49 +0300

    change common controller to rest controller

--- src/main/java/ru/serji/kuklyasha/web/DollController.java
+++ src/main/java/ru/serji/kuklyasha/web/DollController.java
@@ -3,22 +3,27 @@ package ru.serji.kuklyasha.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.serji.kuklyasha.model.Doll;
 import ru.serji.kuklyasha.service.DollService;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 
 import static ru.serji.kuklyasha.util.ValidationUtil.assureIdConsistent;
 import static ru.serji.kuklyasha.util.ValidationUtil.checkNew;
 
-@Controller
-@RequestMapping("/dolls")
+@RestController
+@RequestMapping(value = DollController.API_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class DollController {
+    static final String API_URL = "/api/dolls";
+
     private final DollService dollService;
 
     @Autowired
@@ -38,20 +43,23 @@ public class DollController {
         return dollService.getAll();
     }
 
-    @PostMapping()
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void create(Doll doll) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Doll> create(@Valid @RequestBody Doll doll) {
         log.info("create doll = {}", doll);
-        Objects.requireNonNull(doll, "meal must not be null");
+        Objects.requireNonNull(doll, "doll must not be null");
         checkNew(doll);
-        dollService.save(doll);
+        Doll created = dollService.save(doll);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(API_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(Doll doll, @PathVariable("id") int id) {
+    public void update(@Valid @RequestBody Doll doll, @PathVariable("id") int id) {
         log.info("update doll = {}", doll);
-        Objects.requireNonNull(doll, "meal must not be null");
+        Objects.requireNonNull(doll, "doll must not be null");
         assureIdConsistent(doll, id);
         dollService.save(doll);
     }
