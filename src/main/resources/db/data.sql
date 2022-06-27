74e701739920476a7823cac30e29b8d9dd89ea09
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-06-27 21:54:30 +0300

    create user table and populate user data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,4 +1,6 @@
 DELETE FROM DOLL;
+DELETE FROM USERS;
+
 INSERT INTO DOLL (name, description, price, image)
 VALUES ('Manyasha', 'Pretty Doll', 100.00, 'doll-1.JPG'),
        ('Paulina', 'Pretty Doll', 100.00, 'doll-2.JPG'),
@@ -8,3 +10,12 @@ VALUES ('Manyasha', 'Pretty Doll', 100.00, 'doll-1.JPG'),
        ('Polina', 'Pretty Doll', 100.00, 'doll-6.JPG'),
        ('Margo', 'Pretty Doll', 100.00, 'doll-7.JPG'),
        ('Marina', 'Pretty Doll', 100.00, 'doll-8.JPG');
+
+INSERT INTO USERS (name, email, password)
+VALUES ('User', 'user@yandex.ru', '{noop}password'),
+       ('Admin', 'admin@gmail.com', '{noop}admin');
+
+INSERT INTO USER_ROLES (role, user_id)
+VALUES ('USER', 1),
+       ('ADMIN', 2),
+       ('USER', 2);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,11 +1,31 @@
-DROP TABLE IF EXISTS doll;
+DROP TABLE IF EXISTS USER_ROLES;
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS DOLL;
 
 CREATE TABLE DOLL
 (
-    id               SERIAL PRIMARY KEY,
-    name             VARCHAR UNIQUE                    NOT NULL,
-    description      VARCHAR                           NOT NULL,
-    price            NUMERIC(8,2)                      NOT NULL,
-    image            VARCHAR                           NOT NULL,
-    date_time        TIMESTAMP           DEFAULT now() NOT NULL
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR UNIQUE          NOT NULL,
+    description VARCHAR                 NOT NULL,
+    price       NUMERIC(8, 2)           NOT NULL,
+    image       VARCHAR                 NOT NULL,
+    created     TIMESTAMP DEFAULT now() NOT NULL
+);
+
+CREATE TABLE USERS
+(
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR                 NOT NULL,
+    email    VARCHAR UNIQUE          NOT NULL,
+    password VARCHAR                 NOT NULL,
+    created  TIMESTAMP DEFAULT now() NOT NULL,
+    enabled  BOOL      DEFAULT TRUE  NOT NULL
+);
+
+CREATE TABLE USER_ROLES
+(
+    user_id INTEGER NOT NULL,
+    role    VARCHAR,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
