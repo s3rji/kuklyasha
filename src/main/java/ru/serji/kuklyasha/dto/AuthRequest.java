b976e6c06c8a1538fa88994d2d917b4e9fc283ee
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-01 23:07:43 +0300

    add auth controller and login user

---
+++ src/main/java/ru/serji/kuklyasha/dto/AuthRequest.java
@@ -0,0 +1,18 @@
+package ru.serji.kuklyasha.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.*;
+
+@Value
+public class AuthRequest {
+
+    @Email
+    @NotBlank
+    @Size(max = 128)
+    String email;
+
+    @NotBlank
+    @Size(min = 5, max = 32)
+    String password;
+}
---
+++ src/main/java/ru/serji/kuklyasha/web/AuthController.java
@@ -0,0 +1,48 @@
+package ru.serji.kuklyasha.web;
+
+import lombok.extern.slf4j.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.*;
+import org.springframework.web.bind.annotation.*;
+import ru.serji.kuklyasha.dto.*;
+import ru.serji.kuklyasha.model.*;
+import ru.serji.kuklyasha.security.jwt.*;
+
+import javax.validation.*;
+
+import static ru.serji.kuklyasha.util.UserUtil.*;
+
+@RestController
+@RequestMapping(value = AuthController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+public class AuthController {
+
+    static final String REST_URL = "/api/auth";
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserTo> login(@RequestBody @Valid AuthRequest request) {
+        log.debug("Authenticating '{}'", request.getEmail());
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+            User user = ((AuthUser) authentication.getPrincipal()).getUser();
+            String token = jwtTokenProvider.createToken(user);
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.AUTHORIZATION, token)
+                    .body(createToFromUser(user));
+        } catch (AuthenticationException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
