065bca9137f052bc4adab54353e97fac31c580c3
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-01 23:00:59 +0300

    config security rules

--- src/main/java/ru/serji/kuklyasha/config/WebSecurityConfig.java
+++ src/main/java/ru/serji/kuklyasha/config/WebSecurityConfig.java
@@ -1,19 +1,72 @@
 package ru.serji.kuklyasha.config;
 
+import com.fasterxml.jackson.databind.*;
+import lombok.*;
 import lombok.extern.slf4j.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
+import org.springframework.http.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.http.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.web.authentication.*;
+import ru.serji.kuklyasha.security.jwt.*;
+import ru.serji.kuklyasha.util.*;
+
+import static ru.serji.kuklyasha.util.UserUtil.*;
 
 @Configuration
 @EnableWebSecurity
 @Slf4j
+@AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String IMAGES_ENDPOINT = "/doll-images/**";
+    private static final String LOGIN_ENDPOINT = "/api/auth/login";
+    private static final String PROFILE_ENDPOINT = "/api/profile";
+    private static final String ADMIN_ENDPOINT = "/api/admin/**";
+    private static final String CATALOG_ENDPOINT = "/api/dolls/**";
+
+    private final JwtTokenFilter filter;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    private void setMapper(ObjectMapper objectMapper) {
+        JsonUtil.setMapper(objectMapper);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(PASSWORD_ENCODER);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().anonymous()
-                .and().httpBasic()
-                .and().csrf().disable();
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(IMAGES_ENDPOINT).permitAll()
+                .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
+                .antMatchers(LOGIN_ENDPOINT).anonymous()
+                .antMatchers(HttpMethod.POST, PROFILE_ENDPOINT).anonymous()
+                .antMatchers(CATALOG_ENDPOINT).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
     }
 }
