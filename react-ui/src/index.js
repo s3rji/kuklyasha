e5d1dcd424f267d04dffe6b14371d9836c43e6e7
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-11 22:07:35 +0300

    add login pages

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import DollStore from "./store/DollStore";
 import NavigationStore from "./store/NavigationStore";
+import UserStore from "./store/UserStore";
 
 export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Context.Provider value={{
+        user: new UserStore(),
         doll: new DollStore(),
         navigation: new NavigationStore()
     }}>
