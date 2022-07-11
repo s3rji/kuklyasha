4df8b58ceaecd9192cbdb72d9c944275e0a4720b
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-11 22:49:51 +0300

    add user api

---
+++ react-ui/src/http/userAPI.js
@@ -0,0 +1,19 @@
+import {$host} from "./index";
+import jwt_decode from "jwt-decode";
+
+export const registration = async (name, email, password) => {
+    const {data} = await $host.post('api/profile', {name, email, password})
+    return data
+}
+
+export const login = async (email, password, rememberMe) => {
+    const {data} = await $host.post('api/auth/login', {email, password, rememberMe})
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
+
+export const check = async () => {
+    const {data} = await $host.get('api/profile/auth' )
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
