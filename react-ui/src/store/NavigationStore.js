b5138edaaeb8cc3c9c850beafbf2cce045bdfbd3
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-11 23:03:46 +0300

    set ref for catalog page

--- react-ui/src/store/NavigationStore.js
+++ react-ui/src/store/NavigationStore.js
@@ -3,7 +3,7 @@ import {makeAutoObservable} from "mobx";
 export default class NavigationStore {
     constructor() {
         this._ways = [
-            {name: 'Каталог', href: '#', id: 1},
+            {name: 'Каталог', href: '/', id: 1},
             {name: 'Конструктор', href: '#', id: 2},
             {name: 'О магазине', href: '#', id: 3},
             {name: 'Контакты', href: '#', id: 4},
