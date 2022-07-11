0797b718d1b973a70574f5b99bd079de1994ec7b
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-11 21:56:16 +0300

    add login route

--- react-ui/src/routes.js
+++ react-ui/src/routes.js
@@ -1,5 +1,6 @@
 import Catalog from "./pages/Catalog";
-import {CATALOG_ROUTE, DOLL_ROUTE} from "./utils/consts";
+import {CATALOG_ROUTE, DOLL_ROUTE, LOGIN_ROUTE} from "./utils/consts";
+import Login from "./pages/Login";
 
 export const publicRoutes = [
     {
@@ -9,5 +10,9 @@ export const publicRoutes = [
     {
         path: DOLL_ROUTE + "/:id",
         Component: Catalog
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Login
     }
 ]
