5628925f622cbb13a25e3e63899b5580c15b0198
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-11 22:43:04 +0300

    add cross-origin

--- src/main/java/ru/serji/kuklyasha/web/user/ProfileController.java
+++ src/main/java/ru/serji/kuklyasha/web/user/ProfileController.java
@@ -21,6 +21,7 @@ import static ru.serji.kuklyasha.util.ValidationUtil.*;
 @RestController
 @RequestMapping(value = ProfileController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProfileController extends AbstractUserController {
 
     static final String REST_URL = "/api/profile";
