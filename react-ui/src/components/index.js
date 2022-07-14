1081b16d0a4ffd5837e2f2044c5f981e8aef0ab5
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-14 22:40:35 +0300

    add new component

--- react-ui/src/components/index.js
+++ react-ui/src/components/index.js
@@ -4,6 +4,7 @@ export { default as Currency } from './Currency';
 export { default as DollItem } from './DollItem';
 export { default as DollList } from './DollList';
 export { default as Identification } from './Identification';
+export { default as Loading } from './Loading';
 export { default as Logo } from './Logo';
 export { default as NavBar } from './NavBar';
 export { default as Navigation } from './Navigation';
