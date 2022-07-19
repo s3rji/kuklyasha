8fc86d3b1e73e5dadc73bcb319d7084513471cb0
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-19 23:11:55 +0300

    add user data for tests

--- src/test/resources/db/data.sql
+++ src/test/resources/db/data.sql
@@ -1,7 +1,18 @@
 DELETE FROM DOLL;
+DELETE FROM USERS;
+
 INSERT INTO DOLL (name, description, price, image)
 VALUES ('Doll1', 'Pretty Doll', 100.00, '/image1'),
        ('Doll2', 'Pretty Doll', 100.00, '/image2'),
        ('Doll3', 'Pretty Doll', 100.00, '/image3'),
        ('Doll4', 'Pretty Doll', 100.00, '/image4'),
        ('Doll5', 'Pretty Doll', 100.00, '/image5');
+
+INSERT INTO USERS (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO USER_ROLES (role, user_id)
+VALUES ('USER', 1),
+       ('ADMIN', 2),
+       ('USER', 2);
--- src/test/resources/db/schema.sql
+++ src/test/resources/db/schema.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS doll;
+DROP TABLE IF EXISTS USER_ROLES;
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS DOLL;
 
 CREATE TABLE DOLL
 (
@@ -9,3 +11,21 @@ CREATE TABLE DOLL
     image       VARCHAR                 NOT NULL,
     created     TIMESTAMP DEFAULT now() NOT NULL
 );
+
+CREATE TABLE USERS
+(
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR                 NOT NULL,
+    email    VARCHAR UNIQUE          NOT NULL,
+    password VARCHAR                 NOT NULL,
+    created  TIMESTAMP DEFAULT now() NOT NULL,
+    enabled  BOOL      DEFAULT TRUE  NOT NULL
+);
+
+CREATE TABLE USER_ROLES
+(
+    user_id INTEGER NOT NULL,
+    role    VARCHAR,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+)
