ee575960da30994e1af76d03d62bbb07403bf977
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-07-28 23:13:14 +0300

    add handler for catching app exception

--- src/main/java/ru/serji/kuklyasha/security/jwt/JwtTokenFilter.java
+++ src/main/java/ru/serji/kuklyasha/security/jwt/JwtTokenFilter.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.*;
 import org.springframework.security.core.context.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.filter.*;
+import org.springframework.web.servlet.*;
+import ru.serji.kuklyasha.error.*;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -16,20 +18,27 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
+    private final HandlerExceptionResolver resolver;
+
     @Autowired
-    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider, @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.resolver = resolver;
     }
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
-        String token = jwtTokenProvider.resolveToken(request);
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication auth = jwtTokenProvider.getAuthentication(token, request);
-            SecurityContextHolder.getContext().setAuthentication(auth);
+        try {
+            String token = jwtTokenProvider.resolveToken(request);
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token, request);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+            filterChain.doFilter(request, response);
+        } catch (AppException ex) {
+            resolver.resolveException(request, response, null, ex);
         }
-        filterChain.doFilter(request, response);
     }
 }
