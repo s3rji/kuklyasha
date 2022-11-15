c82f857c7815acb2bae1257e5535d68483bd5129
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-11-15 15:08:05 +0300

    fix order tests when add param user

--- src/test/java/ru/serji/kuklyasha/service/OrderServiceTest.java
+++ src/test/java/ru/serji/kuklyasha/service/OrderServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 import org.springframework.transaction.annotation.*;
-import ru.serji.kuklyasha.*;
 import ru.serji.kuklyasha.error.*;
 import ru.serji.kuklyasha.model.Order;
 import ru.serji.kuklyasha.model.*;
@@ -29,69 +28,85 @@ class OrderServiceTest {
 
     @Test
     void get() {
-        Order actual = orderService.get(ORDER_ID).get();
+        Order actual = orderService.get(ORDER_ID, user).get();
         ORDER_MATCHER.assertMatch(actual, order);
         User actualUser = (User) Hibernate.unproxy(actual.getUser());
-        USER_MATCHER.assertMatch(actualUser, UserTestData.user);
+        USER_MATCHER.assertMatch(actualUser, user);
     }
 
     @Test
     void notFound() {
-        assertTrue(orderService.get(NOT_FOUND).isEmpty());
+        assertTrue(orderService.get(NOT_FOUND, user).isEmpty());
+    }
+
+    @Test
+    void notFoundWithWrongUser() {
+        assertTrue(orderService.get(ORDER_ID, admin).isEmpty());
     }
 
     @Test
     void getAllByUser() {
-        List<Order> actual = orderService.getAllByUser(UserTestData.user);
+        List<Order> actual = orderService.getAll(user);
         ORDER_MATCHER.assertMatch(actual, order);
         actual.forEach(order -> {
             User actualUser = (User) Hibernate.unproxy(order.getUser());
-            USER_MATCHER.assertMatch(actualUser, UserTestData.user);
+            USER_MATCHER.assertMatch(actualUser, user);
         });
     }
 
     @Test
-    void notFoundByUser() {
-        assertTrue(orderService.getAllByUser(UserTestData.admin).isEmpty());
+    void notFoundAllByUser() {
+        assertTrue(orderService.getAll(admin).isEmpty());
     }
 
     @Test
     void create() {
-        Order created = orderService.save(getNew());
+        Order created = orderService.save(getNew(), user);
         int newId = created.id();
         Order newOrder = getNew();
         newOrder.setId(newId);
         ORDER_MATCHER.assertMatch(created, newOrder);
         USER_MATCHER.assertMatch(created.getUser(), newOrder.getUser());
-        ORDER_MATCHER.assertMatch(orderService.get(newId).get(), newOrder);
+        ORDER_MATCHER.assertMatch(orderService.get(newId, user).get(), newOrder);
     }
 
     @Test
     void createInvalid() {
         Order newOrder = getNew();
         newOrder.setStatus(null);
-        assertThrows(ConstraintViolationException.class, () -> orderService.save(newOrder));
+        assertThrows(ConstraintViolationException.class, () -> orderService.save(newOrder, user));
     }
 
     @Test
     void update() {
         Order updated = getUpdated();
-        orderService.save(updated);
-        Order actual = orderService.get(ORDER_ID).get();
+        orderService.save(updated, user);
+        Order actual = orderService.get(ORDER_ID, user).get();
         ORDER_MATCHER.assertMatch(actual, updated);
         User actualUser = (User) Hibernate.unproxy(actual.getUser());
-        USER_MATCHER.assertMatch(actualUser, UserTestData.user);
+        USER_MATCHER.assertMatch(actualUser, user);
+    }
+
+    @Test
+    void updateInvalidWithWrongUser() {
+        Order updated = getUpdated();
+        assertThrows(IllegalRequestDataException.class, () -> orderService.save(updated, admin));
     }
 
     @Test
     void delete() {
-        orderService.delete(ORDER_ID);
-        assertThrows(IllegalRequestDataException.class, () -> orderService.delete(ORDER_ID));
-        assertTrue(orderService.get(ORDER_ID).isEmpty());
+        orderService.delete(ORDER_ID, user);
+        assertThrows(IllegalRequestDataException.class, () -> orderService.delete(ORDER_ID, user));
+        assertTrue(orderService.get(ORDER_ID, user).isEmpty());
     }
 
     @Test
     void deleteNotFound() {
-        assertThrows(IllegalRequestDataException.class, () -> orderService.delete(NOT_FOUND));
+        assertThrows(IllegalRequestDataException.class, () -> orderService.delete(NOT_FOUND, user));
+    }
+
+    @Test
+    void deleteNotFoundWithWrongUser() {
+        assertThrows(IllegalRequestDataException.class, () -> orderService.delete(ORDER_ID, admin));
     }
 }
