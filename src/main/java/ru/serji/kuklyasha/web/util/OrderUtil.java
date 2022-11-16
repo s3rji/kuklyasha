cb8fb356a785f51eca9753d57e60334b6725a098
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2022-11-16 15:32:32 +0300

    check non null param

--- src/main/java/ru/serji/kuklyasha/web/util/OrderUtil.java
+++ src/main/java/ru/serji/kuklyasha/web/util/OrderUtil.java
@@ -1,17 +1,18 @@
 package ru.serji.kuklyasha.web.util;
 
 import lombok.experimental.*;
+import org.springframework.lang.*;
 import ru.serji.kuklyasha.dto.*;
 import ru.serji.kuklyasha.model.*;
 
 @UtilityClass
 public class OrderUtil {
 
-    public static Order createOrderFromTo(OrderTo orderTo, User user) {
+    public static Order createOrderFromTo(@NonNull OrderTo orderTo, @NonNull User user) {
         return new Order(orderTo.getId(), user, orderTo.getItems(), orderTo.getStatus(), orderTo.getTotal());
     }
 
-    public static OrderTo createToFromOrder(Order order) {
-        return new OrderTo(order.id(), order.getItems(), order.getStatus(), order.getTotal());
+    public static OrderTo createToFromOrder(@NonNull Order order) {
+        return new OrderTo(order.getId(), order.getItems(), order.getStatus(), order.getTotal());
     }
 }
