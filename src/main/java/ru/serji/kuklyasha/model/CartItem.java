a395a4ffd953cf454638a5fa0ed1325739a5264f
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2023-02-07 16:09:19 +0300

    add cart item entity and it's service

---
+++ src/main/java/ru/serji/kuklyasha/model/CartItem.java
@@ -0,0 +1,37 @@
+package ru.serji.kuklyasha.model;
+
+import lombok.*;
+import org.hibernate.validator.constraints.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "cart_item", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "doll_id"}, name = "uk_user_doll"))
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem extends BaseEntity {
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "doll_id", nullable = false, updatable = false)
+    @NotNull
+    private Doll doll;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    @NotNull
+    private User user;
+
+    @Column(name = "quantity", nullable = false, columnDefinition = "integer default 1")
+    @Setter
+    @NotNull
+    @Range(min = 1)
+    private int quantity;
+
+    public CartItem(Integer id, Doll doll, User user, int quantity) {
+        super(id);
+        this.doll = doll;
+        this.user = user;
+        this.quantity = quantity;
+    }
+}
