a0676723b644b7ff44f957b4de729df3d89a5657
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2023-06-07 19:29:22 +0300

    refactoring doll item frame

--- react-ui/src/components/DollItem.js
+++ react-ui/src/components/DollItem.js
@@ -6,16 +6,16 @@ const DollItem = ({doll}) => {
     const navigate = useNavigate()
 
     return (
-        <div key={doll.id} className="group" onClick={() => navigate(DOLL_ROUTE + "/" + doll.id)}>
-            <div
-                className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+        <div className="group">
+            <div onClick={() => navigate(DOLL_ROUTE + "/" + doll.id)}
+                 className="h-80 w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <img
                     src={process.env.REACT_APP_IMAGES_URL + doll.poster}
                     alt={doll.description}
                     className="w-80 h-85 object-center object-cover group-hover:opacity-75"
                 />
             </div>
-            <h3 className="mt-4 text-sm text-gray-700">{doll.name}</h3>
+            <h3 className="mt-4 text-sm text-gray-700">{doll.name.length < 20 ? doll.name : doll.name.substring(0, 20) + '...'}</h3>
             <p className="mt-1 text-base font-medium text-gray-900">{doll.price + " RUB"}</p>
         </div>
     );
