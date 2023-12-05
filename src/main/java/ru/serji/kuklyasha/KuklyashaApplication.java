182ef8e4818301d1035136be39de4cd88ed147e9
Sergey Gusarov <mr.gusarov.sergey@gmail.com>
2023-12-05 17:20:28 +0300

    add config server settings to get app configuration

--- src/main/java/ru/serji/kuklyasha/KuklyashaApplication.java
+++ src/main/java/ru/serji/kuklyasha/KuklyashaApplication.java
@@ -2,11 +2,13 @@ package ru.serji.kuklyasha;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.context.config.annotation.*;
 import org.springframework.cloud.stream.annotation.*;
 import org.springframework.cloud.stream.messaging.*;
 
 @SpringBootApplication
 @EnableBinding(Source.class)
+@RefreshScope
 public class KuklyashaApplication {
 
     public static void main(String[] args) {
